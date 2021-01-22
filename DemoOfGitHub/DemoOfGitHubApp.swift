01a7154a6e70e99c67dc109c7aa38ebe8c6322bf
mrcormell <d.cormell@etoncollege.org.uk>
2021-01-22 11:58:31 +0000

    Initial Commit

---
+++ DemoOfGitHub/DemoOfGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoOfGitHubApp.swift
+//  DemoOfGitHub
+//
+//  Created by David Cormell on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoOfGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

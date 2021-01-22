06160fc098f4715dc9e8de0c05eb6fe260b9dbde
mrcormell <d.cormell@etoncollege.org.uk>
2021-01-22 12:02:17 +0000

    Kindness

--- DemoOfGitHub/ContentView.swift
+++ DemoOfGitHub/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Aasmaan and Kit!")
+        Text("Hello, Aasmaan and Kit - great to see you!")
             .padding()
     }
 }
